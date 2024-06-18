1088f0613af9b6cdb978b3b29c21a6cdda32a819
123soikot <162465476+123soikot@users.noreply.github.com>
2024-06-18 19:32:09 +0630

    Update out.js

--- Priyansh/commands/out.js
+++ Priyansh/commands/out.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "leave",
+  name: "out",
   version: "1.0.0",
   hasPermssion: 2,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
@@ -14,6 +14,6 @@ module.exports.run = async function({ api, event, args }) {
    let namee = await api.getThreadInfo(tid)
   if (!tid) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
 
-else return api.removeUserFromGroup(api.getCurrentUserID(), tid, () => api.sendMessage("The bot has left this group", event.threadID, event.messageID));
+else return api.removeUserFromGroup(api.getCurrentUserID(), tid, () => api.sendMessage("love you all,আসসালামু আলাইকুম🥰 আপনাদের সবার সাথে অনেক মজা করলাম যদি কোন ভুল হয়ে থাকে মাফ করে দিবেন😍🥰 বস  এর আদেশ আমি আর এখন কথা বলতে পারবো না😥 আপনাদের সাথে আমার আবার কথা হবে যখন আমার স্যার Ex 卝 বয়ফ্রেন্ডヅ SIR অনুমতি দিবে😍 তাই এখন এর জন্য বিদায়💔!!!", ", event.threadID, event.messageID));
 
 }
